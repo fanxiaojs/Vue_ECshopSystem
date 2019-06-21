ba9537021dadea4b86550350f8399a0cb56b6e92
樊潇 <632790063@qq.com>
2019-06-21 21:05:17 +0800

    完成了home界面侧边栏页面渲染

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,5 +1,7 @@
 <template>
+  <!-- 页面容器 -->
   <el-container>
+    <!-- 头部区域 -->
     <el-header>
       <el-row>
         <el-col :span="6">
@@ -16,7 +18,67 @@
       </el-row>
     </el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <!-- 侧边栏区域 -->
+      <el-aside width="200px">
+        <el-menu class="el-menu-vertical-demo">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              用户列表
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+              <i class="el-icon-menu"></i>角色列表
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-menu"></i>权限列表
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="3-1">
+              <i class="el-icon-menu"></i>商品列表
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <i class="el-icon-menu"></i>分类参数
+            </el-menu-item>
+            <el-menu-item index="3-3">
+              <i class="el-icon-menu"></i>商品分类
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="4-1">
+              <i class="el-icon-menu"></i>订单列表
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="5-1">
+              <i class="el-icon-menu"></i>数据报表
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 内容区域 -->
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -44,15 +106,17 @@ export default {};
   font-weight: 900;
 }
 .el-header .exit {
+  float: right;
   font-size: 16px;
   text-decoration: none;
   color: palevioletred;
+  margin-right: 150px;
 }
 
 .el-aside {
   background-color: #d3dce6;
   color: #333;
-  text-align: center;
+
   line-height: 200px;
 }
 
@@ -63,18 +127,6 @@ export default {};
   line-height: 160px;
 }
 
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
-}
 .el-container {
   height: 100%;
 }
