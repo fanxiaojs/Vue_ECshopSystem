8f76491b75af58c09e2e444d25e4834defcf72f5
樊潇 <632790063@qq.com>
2019-06-24 20:16:36 +0800

    完成了权限管理权限列表数据获取

--- src/components/rights/rights.vue
+++ src/components/rights/rights.vue
@@ -4,11 +4,13 @@
     <!-- 使用自己封装好的面包屑导航 -->
     <Bread one="权限管理" two="权限列表"></Bread>
     <!-- 表格区域 -->
-    <el-table style="width: 100%">
+    <el-table :data="rights" style="width: 100%">
       <el-table-column type="index"></el-table-column>
-      <el-table-column prop="username" label="权限名称" width="180"></el-table-column>
-      <el-table-column prop="email" label="路径" width="180"></el-table-column>
-      <el-table-column prop="mobile" label="层级"></el-table-column>
+      <el-table-column prop="anthName" label="权限名称" width="180"></el-table-column>
+      <el-table-column prop="path" label="路径" width="180"></el-table-column>
+      <el-table-column label="层级">
+        <template slot-scope="scope">{{scope.row.level=="0"?'一级':scope.row.level=="1"?'二级':'三级'}}</template>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -20,6 +22,29 @@ import Bread from "../layout/bread";
 export default {
   components: {
     Bread
+  },
+  data() {
+    return {
+      rights: []
+    };
+  },
+  methods: {
+    getRights() {
+      this.$http({
+        methods: "get",
+        url: "rights/list"
+      }).then(res => {
+        let { data, meta } = res.data;
+        if (meta.status == 200) {
+          this.rights = data;
+        } else {
+          this.$message.error(meta.msg);
+        }
+      });
+    }
+  },
+  mounted() {
+    this.getRights();
   }
 };
 </script>
