810ac2227f0e509f6666381c1b88f482477ea04c
樊潇 <632790063@qq.com>
2019-06-25 20:50:50 +0800

    完成了权限管理角色列表展开项删除权限功能

--- src/components/roles/roles.js
+++ src/components/roles/roles.js
@@ -26,6 +26,29 @@ export default {
           this.$message.error(meta.msg)
         }
       })
+    },
+    //删除权限
+    delRights(rid, rightid, aaa) {
+
+
+      this.$http({
+        method: 'delete',
+        url: `roles/${rid}/rights/${rightid}`
+      }).then(res => {
+        let {
+          meta,
+          data
+        } = res.data
+        if (meta.status == 200) {
+          this.$message({
+            message: meta.msg,
+            type: 'success'
+          })
+          aaa.row.children = data
+        } else {
+          this.$message.error(meta.msg)
+        }
+      })
     }
   },
   mounted() {
